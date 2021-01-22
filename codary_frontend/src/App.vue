c233c52baa92537d159286fbf354edcf2e5f0060
qqyurr <byyulli16@gmail.com>
2021-01-22 17:47:18 +0900

    feat : ui main, nav

--- codary_frontend/src/App.vue
+++ codary_frontend/src/App.vue
@@ -1,53 +1,18 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <h1>메롱</h1>
+  <div id="app"
+  class="d-flex flex-row">
+      <!-- <Memo/> -->
       <Nav/>
-      <Memo/>
-    </v-main>
-  </v-app>
+      <Home/>
+ 
+  </div>
+  
 </template>
 
 <script>
 import Nav from './components/Nav';
-import Memo from './components/Memo';
+import Home from './pages/Home.vue';
+// import Memo from './components/Memo';
 
 
 export default {
@@ -55,7 +20,8 @@ export default {
 
   components: {
     Nav,
-    Memo,
+    Home,
+    // Memo,
   },
 
   data: () => ({
--- codary_frontend/src/components/Nav.vue
+++ codary_frontend/src/components/Nav.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-    <h1>Nav</h1>
+ 
+      <v-app-bar
+        app
+        color="white"
+        height="100"
+      >
+        <v-avatar
+          class="mr-3"
+          color="grey lighten-5"
+          size="70"
+        >
+          <v-img
+            contain
+            max-height="70%"
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          ></v-img>
+        </v-avatar>
+
+        <v-toolbar-title class="font-weight-black headline">
+          VUETIFY
+        </v-toolbar-title>
+      </v-app-bar>
+
   </div>
 </template>
 
