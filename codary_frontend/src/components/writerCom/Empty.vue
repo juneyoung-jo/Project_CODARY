cc31fde1802a842f0a8d86ad1c06ef942f56e16e
coffeii <gsu2520@naver.com>
2021-01-23 22:48:37 +0900

    feat markdown

--- codary_frontend/src/components/writerCom/Empty.vue
+++ codary_frontend/src/components/writerCom/Empty.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
-
+<!--  마크다운 구현예정 -->
+    <!-- <editor 
+      :initialValue="editorText"
+      :options="editorOptions"
+      height="500px"
+      initialEditType="markdown"
+      previewStyle="vertical"
+    /> -->
   </div>
 </template>
 
 <script>
+// import '@toast-ui/editor/dist/toastui-editor.css'
+// import 'codemirror/lib/codemirror.css'
+// import { Editor } from '@toast-ui/vue-editor'
+
 export default {
   name: 'Empty',
+  //   components: {
+  //   'editor': Editor,
+  //   // 'viewer': Viewer
+  // },
+  // data() {
+  //   return {
+  //     editorText: 'This is initialValue.',
+  //     editorOptions: {
+  //       hideModeSwitch: true
+  //     }
+  //   };
+  // },
 }
 </script>
 
