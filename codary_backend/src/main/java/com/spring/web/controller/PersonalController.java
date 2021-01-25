e0c9957478e44317ec65609e51d55519e1dbd72d
wngytkfkd <wngytkfkd@naver.com>
2021-01-25 14:33:38 +0900

    feat : personal controller 작업(3), 블로거가 쓴 게시글 완성

--- codary_backend/src/main/java/com/spring/web/controller/PersonalController.java
+++ codary_backend/src/main/java/com/spring/web/controller/PersonalController.java
@@ -31,22 +31,9 @@ public class PersonalController {
 		HttpStatus status=HttpStatus.ACCEPTED;
 		List<BlogContentsDto> blogcontentsList=null;
 		
-	//	if(jwtService.isUsable(request.getHeader("access-token"))) { //사용가능한 토큰 //내가 쓴 글
-			try {
-				blogcontentsList=personalService.personalContents(blogid);
-				status=HttpStatus.ACCEPTED;
-			}catch(Exception e) {
-				e.printStackTrace();
-				status=HttpStatus.INTERNAL_SERVER_ERROR;
-			}
-	//	}else { //사용불가능한 토큰	//타인블로거가 쓴 글
-			status=HttpStatus.ACCEPTED;
-			String sessionId=session.getAttribute("blogid").toString();
-			//UserDto user = userService.findblog(sessionId);
-			blogcontentsList= personalService.personalContents(sessionId);
-			
-	//	}
+		blogcontentsList= personalService.personalContents(blogid);
 		
 		return new ResponseEntity<List<BlogContentsDto>>(blogcontentsList, HttpStatus.OK);
 	}
+	
 }
