dec362840a11d2b1837b1462ee7cc151863226ed
coffeii <gsu2520@naver.com>
2021-01-27 01:07:53 +0900

    feat 코멘트창만듬

--- codary_frontend/src/components/createPostCom/Editor.vue
+++ codary_frontend/src/components/createPostCom/Editor.vue
@@ -10,6 +10,20 @@
       height="500px"
       initialEditType="markdown"
       previewStyle="vertical"
+      @keypress.enter="datachange()"
+    />
+    <v-btn
+      @click="datachange()"
+    >
+      눌러
+
+    </v-btn>
+    
+    <viewer 
+      :value="editorText" 
+      height="500px"
+      :initialValue="editorText" 
+      viewer=true 
     />
   </div>
 </template>
@@ -17,21 +31,29 @@
 <script>
 import '@toast-ui/editor/dist/toastui-editor.css'
 import 'codemirror/lib/codemirror.css'
-import { Editor } from '@toast-ui/vue-editor'
+import { Editor, Viewer } from '@toast-ui/vue-editor'
 
 export default {
   name: 'Editor',
     components: {
     'editor': Editor,
-    // 'viewer': Viewer
+    'viewer': Viewer
   },
   data() {
     return {
-      editorText: 'This is initialValue.',
+      editorText: '# This is initialValue.',
       editorOptions: {
-        hideModeSwitch: true
+        hideModeSwitch: false
       }
     };
+  },
+  methods: {
+    datachange : function() {
+      
+      this.editorText = (Editor.getMarkdown())
+      console.log(this.editorText)
+      
+    }
   },
 }
 </script>
