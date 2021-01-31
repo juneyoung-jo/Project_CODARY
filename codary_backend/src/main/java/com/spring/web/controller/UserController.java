57606d803425ed7bff4e19927e17029ff30de4c2
gisung2215 <wjv2215@naver.com>
2021-01-31 21:52:57 +0900

    feat: google 로그인 구현

--- codary_backend/src/main/java/com/spring/web/controller/UserController.java
+++ codary_backend/src/main/java/com/spring/web/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.spring.web.dto.UserDto;
 import com.spring.web.service.JwtServiceImpl;
 import com.spring.web.service.UserService;
+import com.spring.web.service.oauth.GoogleOauthService;
 import com.spring.web.service.oauth.KakaoOauthService;
 import com.spring.web.service.oauth.NaverOauthService;
 import com.spring.web.service.oauth.SocialOauth;
@@ -30,6 +31,8 @@ public class UserController {
 	@Autowired
 	private NaverOauthService naverOauthService;
 	@Autowired
+	private GoogleOauthService googleOauthService;
+	@Autowired
 	private JwtServiceImpl jwtService;
 
 	public static final Logger logger = LoggerFactory.getLogger(UserController.class);
@@ -43,6 +46,8 @@ public class UserController {
 			@RequestParam("code") String code) {
 		
 		System.out.println("#"+socialLoginType+ " 로그인 요청받음!!!");
+		System.out.println(socialLoginType);
+		System.out.println(code);
 		SocialOauth socialOauth = getSocialOauth(socialLoginType);
 		String accessTocken = socialOauth.getAccessToken(socialLoginType, code);
 		
@@ -51,6 +56,7 @@ public class UserController {
 
 		UserDto user = null;
 		Map<String, Object> resultMap = new HashMap<>();
+		
 		try {
 			user = userService.findByProvider(userInfo);
 			resultMap.put("message", SUCCESS);
@@ -82,6 +88,8 @@ public class UserController {
 			return kakaoOauthService;
 		case "naver":
 			return naverOauthService;
+		case "google":
+			return googleOauthService;
 		default:
 			return null;
 		}
