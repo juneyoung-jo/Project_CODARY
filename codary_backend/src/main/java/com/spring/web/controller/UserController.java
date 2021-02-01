39b3a96186d89eef4f68f6854a0fe92d5e8522b1
gisung2215 <wjv2215@naver.com>
2021-02-01 22:40:51 +0900

    feat: 카카오 로그인 프런트 연동

--- codary_backend/src/main/java/com/spring/web/controller/UserController.java
+++ codary_backend/src/main/java/com/spring/web/controller/UserController.java
@@ -8,8 +8,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +25,9 @@ import com.spring.web.service.oauth.KakaoOauthService;
 import com.spring.web.service.oauth.NaverOauthService;
 import com.spring.web.service.oauth.SocialOauth;
 
+@CrossOrigin(origins = { "*" })
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
 	@Autowired
@@ -39,7 +45,6 @@ public class UserController {
 	private static final String SUCCESS = "success";
 	private static final String FAIL = "fail";
 
-	
 	@GetMapping("/login/{socialLoginType}")
 	public ResponseEntity<Map<String, Object>> loginByKakao(
 			@PathVariable("socialLoginType") String socialLoginType,
@@ -82,6 +87,48 @@ public class UserController {
 		return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.ACCEPTED);
 	}
 
+	@PostMapping("/login/{socialLoginType}")
+	public ResponseEntity<Map<String, Object>> access(
+			@PathVariable("socialLoginType") String socialLoginType,
+			@RequestBody String accessTocken){
+		
+		System.out.println("accessTocken : " + accessTocken);
+		System.out.println("여기 호출됨");
+		SocialOauth socialOauth = getSocialOauth(socialLoginType);
+		
+		HashMap<String, Object> userInfo = socialOauth.getUserInfoFromOauth(accessTocken);
+		logger.info("#Get userInfo: {}", userInfo);
+
+		UserDto user = null;
+		Map<String, Object> resultMap = new HashMap<>();
+		
+		try {
+			user = userService.findByProvider(userInfo);
+			resultMap.put("message", SUCCESS);
+			if (user == null) {
+				logger.info("#최초 로그인입니다.");
+				resultMap = userService.save(userInfo);
+			} else {
+				logger.info("#기존회원입니다.");
+				resultMap.put("user", user);
+				resultMap.put("userInfo", userService.findUserInfoById(user.getUid()));
+				resultMap.put("blog", userService.findBlogById(user.getBlogId()));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			resultMap.put("message", e.getMessage());
+		}
+		user = (UserDto) resultMap.get("user");
+
+		String token = jwtService.create("uId", user.getUid(), "access-token");
+		logger.debug("#토큰정보: " + token);
+		resultMap.put("access-token", token);
+
+		return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.ACCEPTED);
+		
+	}
+	
+	
 	public SocialOauth getSocialOauth(String socialLoginType) {
 		switch (socialLoginType) {
 		case "kakao":
