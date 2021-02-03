9bc517c5e9e4312fcef6a3e07d56d77d2eec47cd
jinyeong <really_real@naver.com>
2021-02-03 22:02:45 +0900

    feat: 검색 창에 추천 게시글 가져와서 뿌리기

--- codary_frontend/src/components/searchCom/PopularTag.vue
+++ codary_frontend/src/components/searchCom/PopularTag.vue
@@ -2,39 +2,35 @@
   <section id="blog">
     <v-container>
       <h2 class="font-weight-bold mb-3 text-uppercase text-start">인기태그</h2>
-      <v-responsive
-        class="mx-auto mb-12"
-        width="56"
-      >
+      <v-responsive class="mx-auto mb-12" width="56">
         <v-divider class="mb-1"></v-divider>
         <v-divider></v-divider>
       </v-responsive>
       <v-row>
         <v-col
-          v-for="({ src, text, title }, i) in articles"
-          :key="i"
+          v-for="{
+            blogContentsCover,
+            blogDatetime,
+            blogContentsTitle,
+            blogContentsId,
+          } in articles"
+          :key="blogContentsId"
           cols="12"
           md="4"
         >
           <v-img
-            :src="src"
+            :src="blogContentsCover"
             class="mb-4"
             height="275"
             max-width="100%"
           ></v-img>
           <h3
             class="font-weight-black mb-4 text-uppercase"
-            v-text="title"
+            v-text="blogContentsTitle"
           ></h3>
-          <div
-            class="title font-weight-light mb-5"
-            v-text="text"
-          ></div>
-          <router-link :to="'/viewpost'" class='noline'>
-            <v-btn
-              class="ml-n4 font-weight-black"
-              text
-            >
+          <div class="title font-weight-light mb-5" v-text="blogDatetime"></div>
+          <router-link :to="'/viewpost'" class="noline">
+            <v-btn class="ml-n4 font-weight-black" text>
               Continue Reading
             </v-btn>
           </router-link>
@@ -47,31 +43,46 @@
 
 <script>
 export default {
-    name:"PopularTag",
-    data () {
-      return {
-        articles: [
-          {
-            src: 'https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-            title: 'Mobile first & Responsive',
-            text: 'Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula eget tortor tempor, quis porta tellus dictum.',
-          },
-          {
-            src: 'https://images.unsplash.com/photo-1475938476802-32a7e851dad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-            title: 'Think outside the box',
-            text: 'Nam ut leo ipsum. Maecenas pretium aliquam feugiat. Aenean vel tempor est, vitae tincidunt risus. Sed sodales vestibulum nibh.',
-          },
-          {
-            src: 'https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80',
-            title: 'Small changes, big difference',
-            text: 'Vestibulum in dictum velit, in rhoncus nibh. Maecenas neque libero, interdum a dignissim in, aliquet vitae lectus. Phasellus lorem enim, luctus ut velit eget.',
-          },
-        ],
-      }
-    }
-}
+  name: "PopularTag",
+  data() {
+    return {
+      // articles: [
+      //   {
+      //     src: 'https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+      //     title: 'Mobile first & Responsive',
+      //     text: 'Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula eget tortor tempor, quis porta tellus dictum.',
+      //   },
+      //   {
+      //     src: 'https://images.unsplash.com/photo-1475938476802-32a7e851dad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+      //     title: 'Think outside the box',
+      //     text: 'Nam ut leo ipsum. Maecenas pretium aliquam feugiat. Aenean vel tempor est, vitae tincidunt risus. Sed sodales vestibulum nibh.',
+      //   },
+      //   {
+      //     src: 'https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80',
+      //     title: 'Small changes, big difference',
+      //     text: 'Vestibulum in dictum velit, in rhoncus nibh. Maecenas neque libero, interdum a dignissim in, aliquet vitae lectus. Phasellus lorem enim, luctus ut velit eget.',
+      //   },
+      // ],
+      articles: [],
+    };
+  },
+  created() {
+    this.getRecommends();
+  },
+  methods: {
+    getRecommends() {
+      this.axios
+        .get(`blog/recommend`)
+        .then((res) => {
+          console.log(res);
+          this.articles = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
