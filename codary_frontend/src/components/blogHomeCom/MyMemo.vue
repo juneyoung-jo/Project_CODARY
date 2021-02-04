7458269902c5e56b6450d1eced7bd2e3963f336f
김주효 <wngytkfkd@naver.com>
2021-02-04 10:57:16 +0900

    feat : MyPost.vue 내 글 불러오기 작업 완성

--- codary_frontend/src/components/blogHomeCom/MyMemo.vue
+++ codary_frontend/src/components/blogHomeCom/MyMemo.vue
@@ -29,29 +29,32 @@
 </template>
 
 <script>
+import { showMyMemo } from '@/api/personal.js';
+
 export default {
-    name:"MyMemo",
-    data () {
-      return {
-        articles: [
-          {
-            src: 'https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-            title: 'Memo',
-            text: 'Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula eget tortor tempor, quis porta tellus dictum.',
-          },
-          {
-            src: 'https://images.unsplash.com/photo-1475938476802-32a7e851dad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-            title: 'Memo2',
-            text: 'Nam ut leo ipsum. Maecenas pretium aliquam feugiat. Aenean vel tempor est, vitae tincidunt risus. Sed sodales vestibulum nibh.',
-          },
-          {
-            src: 'https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80',
-            title: 'Memo3',
-            text: 'Vestibulum in dictum velit, in rhoncus nibh. Maecenas neque libero, interdum a dignissim in, aliquet vitae lectus. Phasellus lorem enim, luctus ut velit eget.',
-          },
-        ],
-      }
+  name:"MyMemo",
+  data () {
+    return {
+      articles: [],
+      userData: {
+        blogId: 'QfIRyFO1HK5H',
+        uId: '2u1wQOyL8StR',
+        memoId: '2u1wQOyL8StR'
+      }, 
     }
+  },
+  created(){
+    showMyMemo(
+      this.userData,
+      (response) => {
+        console.log(response)
+        this.articles = response.data
+      },
+      (err) => {
+        console.log(err)
+      }
+    )        
+},
 }
 </script>
 
