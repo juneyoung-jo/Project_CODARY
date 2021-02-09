46594303cd5dda7463f02764e2a561c3ed2ad1e5
jinyeong <really_real@naver.com>
2021-02-10 03:23:42 +0900

    refactor: 검색 로직 수정

--- codary_backend/src/main/java/com/spring/web/service/SearchPostService.java
+++ codary_backend/src/main/java/com/spring/web/service/SearchPostService.java
@@ -1,19 +1,12 @@
 package com.spring.web.service;
 
 import java.util.List;
-import java.util.Map;
-
-import org.springframework.http.ResponseEntity;
 
 import com.spring.web.dto.BlogPostDto;
-import com.spring.web.dto.SearchParam;
 
 public interface SearchPostService {
-	List<BlogPostDto> searchPost(SearchParam param) throws Exception;
-	List<BlogPostDto> orderByDate(String keyword) throws Exception;
-	List<BlogPostDto> orderByDate() throws Exception;
-	List<BlogPostDto> orderByView(String keyword) throws Exception;
-	List<BlogPostDto> orderByView() throws Exception;
-	List<BlogPostDto> orderByLike(String keyword) throws Exception;
-	List<BlogPostDto> orderByLike() throws Exception;
+//	List<BlogPostDto> searchPost(SearchParam param) throws Exception;
+	List<BlogPostDto> searchByHash(List<Integer> keywords) throws Exception;
+	List<BlogPostDto> searchByTitle(String keyword) throws Exception;
+	List<BlogPostDto> searchAll() throws Exception;
 }
