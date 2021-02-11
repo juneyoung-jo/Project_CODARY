55a6de67cd32ccc069eb7c981b15e62483e2b43f
young <whwnsdud2000@naver.com>
2021-02-11 19:14:02 +0900

    feat : 블로그 글 작성 시 이미지 처리 기능 완료

--- codary_frontend/src/components/blogHomeCom/Profile.vue
+++ codary_frontend/src/components/blogHomeCom/Profile.vue
@@ -1,166 +1,121 @@
 <template>
-<div class='py-12'>
-  <v-container
-    id="user-profile-view"
-    fluid
-    tag="section"
-  >
-    <v-row>
-      <v-col
-        cols="12"
-      >
-        <v-img
-          class="rounded-circle elevation-6"
-          :src="this.loggedInUserData.profile"
-          width="128"
-        />
-      
-        <v-card-text>
-          <h6 class="text-h6 mb-2 text--secondary">
-            CEO / FOUNDER
-          </h6>
+  <div class="py-12">
+    <v-container id="user-profile-view" fluid tag="section">
+      <v-row>
+        <v-col cols="12">
+          <v-img
+            class="rounded-circle elevation-6"
+            :src="this.loggedInUserData.profile"
+            width="128"
+          />
 
-          <h4 class="text-h4 mb-3 text--primary">
-            {{ this.loggedInUserData.nickname }}
-          </h4>
+          <v-card-text>
+            <h6 class="text-h6 mb-2 text--secondary">
+              CEO / FOUNDER
+            </h6>
 
-          <p class="text--secondary">
-            <v-card flat>
-              <v-card-text>
-                <v-container fluid class="pa-0">
-                  <v-row>
-                    <v-col cols="6" sm="3">
-                      팔로우
-                      <template v-if="blogerLikeflag">
-                        <v-btn icon color="pink" @click="blgUnlike()">
-                          <v-icon>mdi-heart</v-icon>
-                        </v-btn>
-                      </template>
-                      <template v-else>
-                        <v-btn icon color="gray" @click="blgLike()">
-                          <v-icon>mdi-heart</v-icon>
-                        </v-btn>
-                      </template>
-                     </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-            </v-card>
-          </p>
-          <div class='d-flex'>
-            <router-link class='noline' :to="'/createpost'">
-              <v-btn
-                color="dark"
-                min-width="100"
-                rounded
-                outlined
-              >
-                글 작성
-              </v-btn>
-            </router-link>
-            <div class='pa-1'></div>
-            <!-- 프로필수정 모달 -->
-            <v-dialog
-              v-model="dialog"
-              persistent
-              max-width="600px"
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  color="dark"
-                  rounded
-                  v-bind="attrs"
-                  v-on="on"
-                  outlined
-                >
-                  프로필수정
-                </v-btn>
-              </template>
-              <v-card>
-                <v-card-title>
-                  <span class="headline">User Profile</span>
-                </v-card-title>
+            <h4 class="text-h4 mb-3 text--primary">
+              {{ this.loggedInUserData.nickname }}
+            </h4>
+
+            <p class="text--secondary">
+              <v-card flat>
                 <v-card-text>
-                  <v-container>
+                  <v-container fluid class="pa-0">
                     <v-row>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                      >
-                        <v-text-field
-                          placeholder="name"
-                          hint="example of persistent helper text"
-                          persistent-hint
-                          required
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12">
-                        <v-text-field
-                          placeholder="job"
-                          required
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12">
-                        <v-text-field
-                          placeholder="intro"
-                          required
-                        ></v-text-field>
-                      </v-col>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                      >
-                      <v-img :src="this.loggedInUserData.profile" alt="profile"></v-img>
-                      </v-col>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                      >
-                        <v-file-input
-                          accept="image/png, image/jpeg, image/bmp"
-                          placeholder="Pick an avatar"
-                          prepend-icon="mdi-camera"
-                        ></v-file-input>
+                      <v-col cols="6" sm="3">
+                        팔로우
+                        <template v-if="blogerLikeflag">
+                          <v-btn icon color="pink" @click="blgUnlike()">
+                            <v-icon>mdi-heart</v-icon>
+                          </v-btn>
+                        </template>
+                        <template v-else>
+                          <v-btn icon color="gray" @click="blgLike()">
+                            <v-icon>mdi-heart</v-icon>
+                          </v-btn>
+                        </template>
                       </v-col>
                     </v-row>
                   </v-container>
                 </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn
-                    color="blue darken-1"
-                    text
-                    @click="dialog = false"
-                  >
-                    Close
-                  </v-btn>
-                  <v-btn
-                    color="blue darken-1"
-                    text
-                    @click="dialog = false"
-                  >
-                    Save
-                  </v-btn>
-                </v-card-actions>
               </v-card>
-            </v-dialog>
-          </div>
-        </v-card-text>
-      </v-col>
-    </v-row>
-  </v-container>
-</div>
+            </p>
+            <div class="d-flex">
+              <router-link class="noline" :to="'/createpost'">
+                <v-btn color="dark" min-width="100" rounded outlined>
+                  글 작성
+                </v-btn>
+              </router-link>
+              <div class="pa-1"></div>
+              <!-- 프로필수정 모달 -->
+              <v-dialog v-model="dialog" persistent max-width="600px">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn color="dark" rounded v-bind="attrs" v-on="on" outlined>
+                    프로필수정
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="headline">User Profile</span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            placeholder="name"
+                            hint="example of persistent helper text"
+                            persistent-hint
+                            required
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field placeholder="job" required></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field placeholder="intro" required></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                          <v-img :src="this.loggedInUserData.profile" alt="profile"></v-img>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                          <v-file-input
+                            accept="image/png, image/jpeg, image/bmp"
+                            placeholder="Pick an avatar"
+                            prepend-icon="mdi-camera"
+                          ></v-file-input>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="dialog = false">
+                      Close
+                    </v-btn>
+                    <v-btn color="blue darken-1" text @click="dialog = false">
+                      Save
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </div>
+          </v-card-text>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
-import { blogerLike, blogerUnlike, readBlogerlike} from '@/api/personal.js';
+import { mapState } from 'vuex';
+import { blogerLike, blogerUnlike, readBlogerlike } from '@/api/personal.js';
 import { getuidCookie, getblogIdCookie } from '@/util/cookie.js';
 
 export default {
   name: 'Profile',
-  created(){
+  created() {
     readBlogerlike(
       this.blogerLike,
       (response) => {
@@ -172,31 +127,31 @@ export default {
       }
     );
   },
-  data() { 
-    return{
+  data() {
+    return {
       dialog: false,
       blogerLike: {
         uid: getuidCookie(),
         blogId: getblogIdCookie(),
       },
       blogerLikeflag: '',
-    }
+    };
   },
   computed: {
-    ...mapState([ 'loggedInUserData' ])    
+    ...mapState(['loggedInUserData']),
   },
   methods: {
-  /*  initUser(){
+    /*  initUser(){
       this.blogerLike.user = getuidCookie();
       this.blogerLike.blogId = getblogIdCookie();
     },*/
-    blgLike(){
+    blgLike() {
       console.log('들어왔당');
       blogerLike(
         this.blogerLike,
         (response) => {
-          if(response.data.msg === 'success'){
-            this.blogerLikeflag=true;
+          if (response.data.msg === 'success') {
+            this.blogerLikeflag = true;
             console.log('좋아요누름');
           }
         },
@@ -205,12 +160,12 @@ export default {
         }
       );
     },
-    blgUnlike(){
+    blgUnlike() {
       console.log('언팔각이다');
       blogerUnlike(
         this.blogerLike,
         (response) => {
-          if(response.data.msg === 'success'){
+          if (response.data.msg === 'success') {
             this.blogerLikeflag = false;
             console.log('언팔누름');
           }
@@ -219,9 +174,7 @@ export default {
           console.log(error);
         }
       );
-    }
-  }
-}
-
-  
+    },
+  },
+};
 </script>
