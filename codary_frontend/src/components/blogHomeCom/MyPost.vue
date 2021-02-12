e443ced3422854f60e4938617016018e7be067eb
김주효 <wngytkfkd@naver.com>
2021-02-12 23:16:45 +0900

    fix : 내가 쓴 글 카드뷰 수정

--- codary_frontend/src/components/blogHomeCom/MyPost.vue
+++ codary_frontend/src/components/blogHomeCom/MyPost.vue
@@ -58,40 +58,91 @@
       <v-div class="subtitle-2 text-center">
         <h4 v-if="this.articles.length==0">작성한 글이 없어요!</h4>
       </v-div>
+
       <v-row>
         <v-col
-          v-for="(article, blogId, blogContentsId) in articles"
+          v-for="{
+            blogContentsCover,
+            blogDatetime,
+            blogContentsTitle,
+            blogContentsLike,
+            blogContentsView,
+            blogContentsId,
+            blogContents,
+            blogId,
+            profile,
+            nickname,
+            commentCnt,
+          } in articles"
           :key="blogContentsId"
           cols="12"
           md="4"
         >
-          <v-img
-            :src=article.blogContentsCover
-            class="mb-4"
-            height="275"
-            max-width="100%"
-          ></v-img>
-          <h3
-            class="font-weight-black mb-4 text-uppercase"
-          >{{article.blogContentsTitle}}</h3>
-          <div
-            class="title font-weight-light mb-5"
-            
-          ></div>
-          <router-link :to="{
+          <router-link
+            :to="{
               name: 'ViewPost',
               query: {
-                blogId: article.blogId,
-                blogContentsId: article.blogContentsId,
+                blogId: blogId,
+                blogContentsId: blogContentsId,
               },
             }"
-             class='noline'>
-            <v-btn
-              class="ml-n4 font-weight-black"
-              text
-            >
+            class="noline rounded-lg"
+          >
+            <v-hover v-slot="{ hover }">
+              <v-card
+                :elevation="hover ? 12 : 2"
+                class="contentCard"
+                style="height:400px; transition:1s;"
+              >
+                <div>
+                  <v-img
+                    :src="blogContentsCover"
+                    class="mb-4"
+                    :style="hover ? { opacity: 0.4 } : { opacity: 1 }"
+                    height="150px"
+                    max-width="100%"
+                  >
+                  </v-img>
+
+                  <div
+                    v-if="hover"
+                    class="d-flex align-center pl-2 white--black "
+                    style="position:absolute; top:0% "
+                    transition="fade-transition"
+                  >
+                    <div class="d-flex align-center pl-2 white--black " style="height:150px">
+                      {{ blogContents | textLengthOverCut }}
+                    </div>
+                  </div>
+                </div>
+                <div style="padding:10px">
+                  <h2 class="font-weight-black mb-3 ml-0" v-text="blogContentsTitle"></h2>
+                  <h4 class="mb-2">태그 들어갈 자리</h4>
+
+                  <div class="d-flex align-center mb-3">
+                    <v-img :src="profile" class="mr-3" height="30" max-width="30px" style="border-radius:15px"></v-img>
+                    <span>
+                      {{ nickname }}
+                    </span>
+                  </div>
+                </div>
+
+                <div class="d-flex align-end flex-column mr-3">
+                  <span class="font-weight-light">
+                    <font-awesome-icon :icon="['fas', 'comment-dots']" />
+                    {{ commentCnt }}
+                    <font-awesome-icon :icon="['fas', 'heart']" class="ml-2" />
+                    {{ blogContentsLike }}
+                    <font-awesome-icon :icon="['fas', 'eye']" class="ml-2" />
+                    {{ blogContentsView }}
+                  </span>
+                  <div class=" font-weight-light justify-end" v-text="blogDatetime"></div>
+                </div>
+                <!-- <v-btn class="ml-n4 font-weight-black" text>
               Continue Reading
-            </v-btn>
+            </v-btn> -->
+              </v-card>
+            </v-hover>
           </router-link>
         </v-col>
       </v-row>
