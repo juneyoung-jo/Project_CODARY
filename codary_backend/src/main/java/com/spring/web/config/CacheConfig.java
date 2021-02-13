81713be4c9a3699e2b1e8aa2c8c6e13aefb84669
young <whwnsdud2000@naver.com>
2021-02-13 20:17:17 +0900

    feat : redis setting

---
+++ codary_backend/src/main/java/com/spring/web/config/CacheConfig.java
@@ -0,0 +1,43 @@
+//package com.spring.web.config;
+//
+//import java.time.Duration;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cache.CacheManager;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.cache.RedisCacheConfiguration;
+//import org.springframework.data.redis.cache.RedisCacheManager;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+//import org.springframework.data.redis.serializer.RedisSerializationContext;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//@Configuration
+//public class CacheConfig {
+//	
+//	 @Autowired
+//	  RedisConnectionFactory redisConnectionFactory;
+//	  
+//	  @Autowired
+//	  ObjectMapper objectMapper;
+//	  
+//	  @Autowired
+//	  RedisConnectionFactory connectionFactory;
+//	  
+//	  
+//	  @Bean
+//	  public CacheManager redisCacheManager() {
+//	    RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+//		  .disableCachingNullValues() //null 값 처리안함.
+//		  .entryTtl(Duration.ofSeconds(60*60)) // 캐시의 기본 유효시간 설정
+//	      .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+//	      .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+//	    
+//	    RedisCacheManager redisCacheManager = RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory).cacheDefaults(redisCacheConfiguration).build();
+//	    return redisCacheManager;
+//	  }
+//
+//}
