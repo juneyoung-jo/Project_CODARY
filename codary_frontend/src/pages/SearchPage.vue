f11dc043792022590b7d47f38ee1854c26357636
jinyeong <really_real@naver.com>
2021-02-15 14:44:02 +0900

    feat: 해시태그 검색 axios

--- codary_frontend/src/pages/SearchPage.vue
+++ codary_frontend/src/pages/SearchPage.vue
@@ -1,29 +1,25 @@
 <template>
   <div class="py-12">
     <v-container>
-      <v-tabs vertical class="d-flex align-center" style="width:80%; ">
-      <v-tab>
-        <v-icon left>
-          mdi-account
-        </v-icon>
-        제목으로 검색
-      </v-tab>
-      <v-tab>
-        <v-icon left>
-          mdi-lock
-        </v-icon>
-        태그로 검색
-      </v-tab>
-       <v-tab-item class="" style="width:100%">
-        <SearchInput @GETKEYWORD="getkeyword" />
-      </v-tab-item>
-      <v-tab-item class="">
-        <SearchInputTag @GETKEYWORD="getkeyword" />
-      </v-tab-item>
+      <v-tabs vertical class="d-flex align-center" style="width: 80%">
+        <v-tab>
+          <v-icon left> mdi-account </v-icon>
+          제목으로 검색
+        </v-tab>
+        <v-tab>
+          <v-icon left> mdi-lock </v-icon>
+          태그로 검색
+        </v-tab>
+        <v-tab-item class="" style="width: 100%">
+          <SearchInput @GETKEYWORD="getkeyword" />
+        </v-tab-item>
+        <v-tab-item class="">
+          <SearchInputTag @GETHASH="gethash" />
+        </v-tab-item>
       </v-tabs>
-   
+
       <div class="d-flex selectsort ma-2">
-        <select class='now' v-model="selected" @change="changeSelect($event)">
+        <select class="now" v-model="selected" @change="changeSelect($event)">
           <option disabled>정렬 기준</option>
           <option
             v-for="option in options"
@@ -32,7 +28,7 @@
           >
             {{ option.text }}
           </option>
-        </select>  
+        </select>
       </div>
       <SearchResult v-if="this.isSearched" :list="list" />
       <Popular-tag v-else :list="list" />
@@ -45,7 +41,14 @@ import PopularTag from "../components/searchCom/PopularTag.vue";
 import SearchResult from "../components/searchCom/SearchResult.vue";
 import SearchInput from "../components/searchCom/SearchInput.vue";
 import SearchInputTag from "../components/searchCom/SearchInputTag.vue";
-import { getByDate, getByView, getByLike } from "@/api/search.js";
+import {
+  getByDate,
+  getByView,
+  getByLike,
+  hashByDate,
+  hashByView,
+  hashByLike,
+} from "@/api/search.js";
 
 export default {
   components: { SearchInput, SearchResult, PopularTag, SearchInputTag },
@@ -61,6 +64,7 @@ export default {
         { text: "좋아요순", value: 3 },
       ],
       list: [],
+      values: [],
       titlesearch: true,
     };
   },
@@ -73,32 +77,55 @@ export default {
       if (this.keyword.length == 0) this.isSearched = false;
       else this.isSearched = true;
       console.log(this.keyword);
+      this.titlesearch = true;
+      this.searchBy();
+    },
+    gethash(values) {
+      this.values = values;
+      // console.log(this.values);
+      this.titlesearch = false;
       this.searchBy();
     },
     changeSelect(event) {
       this.selected = event.target.value;
+      this.titlesearch = true;
       this.searchBy();
     },
     searchBy() {
-      if (this.selected == 2)
-        getByView(this.keyword, (res) => {
-          this.list = res.data;
-        });
-      else if (this.selected == 3)
-        getByLike(this.keyword, (res) => {
-          this.list = res.data;
-        });
-      else
-        getByDate(this.keyword, (res) => {
-          this.list = res.data;
-        });
+      if (this.selected == 2) {
+        if (this.titlesearch)
+          getByView(this.keyword, (res) => {
+            this.list = res.data;
+          });
+        else
+          hashByView(this.values, (res) => {
+            this.list = res.data;
+          });
+      } else if (this.selected == 3) {
+        if (this.titlesearch)
+          getByLike(this.keyword, (res) => {
+            this.list = res.data;
+          });
+        else
+          hashByLike(this.values, (res) => {
+            this.list = res.data;
+          });
+      } else {
+        if (this.titlesearch)
+          getByDate(this.keyword, (res) => {
+            this.list = res.data;
+          });
+        else
+          hashByDate(this.values, (res) => {
+            this.list = res.data;
+          });
+      }
     },
   },
 };
 </script>
 
 <style scoped>
-
 .selectsort select {
   padding: 12px;
   width: 200px;
@@ -113,7 +140,7 @@ export default {
 
 .selectsort select option {
   padding: 10px;
-  color:black
+  color: black;
 }
 
 .now {
