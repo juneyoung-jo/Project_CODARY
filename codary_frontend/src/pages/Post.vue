ce94d11a8641cdbc1881800a24995d2cf4409396
coffeii <gsu2520@naver.com>
2021-02-18 13:40:46 +0900

    fix : finally???

--- codary_frontend/src/pages/Post.vue
+++ codary_frontend/src/pages/Post.vue
@@ -5,7 +5,22 @@
       <!-- <PostViewer :blogContents="blogContents" /> -->
       <v-sheet color="white" class="ma-10 pa-4 my-10 mt-8">
         <div class="pt-14 mt-8 pa-12 ml-16 keepblack">
-          <h1 class="mb-10 mx-10 mt-8">{{ this.blogContents.blogContentsTitle }}</h1>
+          <h1 class="mb-10 mx-10 mt-8">
+            {{ this.blogContents.blogContentsTitle }}
+            <v-btn v-if="this.likeOrNot" fab class="ml-4" color="yellow" align="center" @click="clickLike()">
+              <span style="font-size:25px">
+                <font-awesome-icon :icon="['fas', 'star']"  />
+              </span>
+             
+            </v-btn>
+            <v-btn v-if="!this.likeOrNot" fab class="ml-4" align="center" @click="clickLike()">
+              <span style="font-size:25px">
+                <font-awesome-icon :icon="['fas', 'star']" />
+              </span>
+              
+            </v-btn>        
+          </h1>
+         
           <div class="ml-10">
             <v-chip
               v-for="hashtag in this.blogContents.hashtags"
@@ -20,14 +35,16 @@
         </div>
         <div class="ma-16 pa-4 mt-1">
           <v-sheet class="pa-13 ml-10" light id="viewer" />
-          <div class="py-3"></div>
+          
           <div class="ml-8">
             <Profile class="ml-10" :blogContents="blogContents" />
-            <v-btn-toggle v-model="icon" class="ml-10 mt-10" dense>
-              <v-btn align="center" @click="clickLike()">
+            <div class="d-flex justify-end">
+
+            <v-btn-toggle v-model="icon" class="" dense>
+              <!-- <v-btn align="center" @click="clickLike()">
                 <font-awesome-icon :icon="['fas', 'thumbs-up']" class="mr-2" />
                 <div>== {{ likeOrNot }}</div>
-              </v-btn>
+              </v-btn> -->
               <v-btn v-if="this.isItMine" id="modify" align="center" @click="editPost()">
                 <font-awesome-icon :icon="['fas', 'edit']" class="mr-2" />
                 수정
@@ -37,8 +54,9 @@
                 삭제
               </v-btn>
             </v-btn-toggle>
+            </div>
           </div>
-          <div class="py-8"></div>
+         
         </div>
       </v-sheet>
 
@@ -314,12 +332,12 @@ export default {
       // console.log(this.user);
       if (this.user.uid === "") alert("로그인 해주세요");
       else if (this.likeOrNot) {
-        if (confirm('좋아요를 취소하시겠어요?')) {
+        if (confirm('즐겨찾기를 취소하시겠어요?')) {
           contentsUnlike(
             this.blogContentsLike,
             () => {
               this.likeOrNot = false;
-              alert('좋아요가 취소되었습니다.');
+              // alert('좋아요가 취소되었습니다.');
             },
             () => {
               alert('다시 시도해주세요.');
